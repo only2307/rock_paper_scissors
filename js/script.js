93aef9ec9d96f23fb84f3f753140cd29fe1d7b47
Hoang-Duy Ho <19120207@student.hcmus.edu.vn>
2021-12-02 08:05:54 +0700

    Add UI v1 done

--- js/script.js
+++ js/script.js
@@ -1,87 +1,84 @@
-function computerPlay() {
-    let choice = ['Rock', 'Paper', 'Scissors'];
-    let selection = choice[Math.floor(Math.random() * choice.length)];
-    return selection;
+let playerSelection;
+let computerSelection = {Value: ""};
+let playerChoiceInt = 0;
+let computerChoiceInt = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+const player = document.querySelector("#player");
+player.textContent = `Player Score: ${playerScore}`;
+
+const computer = document.querySelector("#computer");
+computer.textContent = `Computer Score: ${computerScore}`;
+
+const output = document.querySelector(".output");
+output.textContent = "Who's the winner? Try a guess before playing!";
+
+const buttons = document.querySelectorAll('.button');
+buttons.forEach((button) => {button.addEventListener('click', () => {
+    playerSelection = button.id;
+    if (playerSelection == "rock") {
+        playerChoiceInt = 0;
+    }
+    else if (playerSelection == "paper") {
+        playerChoiceInt = 1;
+    }
+    else if (playerSelection == "scissors") {
+        playerChoiceInt = 2;
+    }
+    computerChoiceInt = computerPlay(computerSelection);
+    game();
+})
+});
+
+function computerPlay(computerSelection) {
+    let selectionNum = Math.floor(Math.random() * 3);
+    if (selectionNum == 0) {
+        computerSelection.Value = "rock";
+    }
+    else if (selectionNum == 1) {
+        computerSelection.Value = "paper";
+    }
+    else if (selectionNum == 2) {
+        computerSelection.Value = "scissors";
+    }
+    return selectionNum;
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    switch(playerSelection) {
-        case 'rock':
-            if (computerSelection == 'rock') {
-                console.log('The result is draw');
-                return 0;
-            }
-            else if (computerSelection == 'scissors') {
-                console.log('You Win! Rock beats Scissors');
-                return 1;
-            }
-            else {
-                console.log('You Lose! Paper beats Rock');
-                return -1;
-            }
-            break;
-        case 'paper':
-            if (computerSelection == 'paper') {
-                console.log('The result is draw');
-                return 0;
-            }
-            else if (computerSelection == 'rock') {
-                console.log('You Win! Paper beats Rock');
-                return 1;
-            }
-            else {
-                console.log('You Lose! Scissors beats Paper');
-                return -1
-            }
-            break;
-        case 'scissors':
-            if (computerSelection == 'scissors') {
-                console.log('The result is draw');
-                return 0;
-            }
-            else if (computerSelection == 'paper') {
-                console.log('You Win! Scissors beats Paper');
-                return 1;
-            }
-            else {
-                console.log('You Lose! Rock beats Scissors');
-                return -1;
-            }
-            break;
-        default:
-            return 'Invalid Input! Run and Type again';
+function playRound() {
+    let matrixWin = [[0, -1, 1], [1, 0, -1], [-1, 1, 0]];
+    let point = matrixWin[playerChoiceInt][computerChoiceInt];
+    if (point == 0) {
+        output.textContent = `It's a tie! Your selection is ${playerSelection} and the computer's selection is ${computerSelection.Value}`;
+    }
+    else if (point == 1) {
+        output.textContent = `You won! Your selection is ${playerSelection} and the computer's selection is ${computerSelection.Value}`;
+        playerScore++;
+    }
+    else if (point == -1) {
+        output.textContent = `You lost! Your selection is ${playerSelection} and the computer's selection is ${computerSelection.Value}`;
+        computerScore++;
     }
 }
 
 function game() {
-    let playerWin = 0, computerWin = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Your selection is: ');
-        const computerSelection = computerPlay();
-        console.log(computerSelection);
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result); 
-        if (result == 1) {
-            playerWin++;
-        }
-        else if (result == -1) {
-            computerWin++;
-        }
-        else {
-            continue;
-        }
+    output.textContent = "Continue! Choosing Rock, Paper, Scissors";
+    playRound();
+
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${computerScore}`;
+    if (playerScore == 5) {
+        output.textContent = "You won the game!";
+        playerScore = 0;
+        computerScore = 0;
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${computerScore}`;
     }
-    if (playerWin > computerWin) {
-        console.log('You win!');
-    }
-    else if (playerWin < computerWin) {
-        console.log('You lose!');
-    }
-    else {
-        console.log("It's draw!")
+    else if (computerScore == 5) {
+        output.textContent = "You lost the game! Do you wanna try again?";
+        playerScore = 0;
+        computerScore = 0;
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${computerScore}`;
     }
 }
-
-game();
